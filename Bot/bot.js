6634ba172e6ad49724aa46e65f000f842de7e042
Sir-Deon <sird655@gmail.com>
2023-11-27 09:52:59 +0100

    made updates

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,14 +2,16 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "6875305967:AAH70nRbXbN4t4RBnwi6cHRDHCJit094bfU";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://isfoody.netlify.app";
+const bot_url= "https://t.me/isfoody_bot."
 
 bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+  ctx.reply("Welcome :))))) \n to foody", {
     reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+      inline_keyboard: [[{ text: "Visit Foody", url: bot_url  }]],
     },
   })
 );
+
 
 bot.launch();
